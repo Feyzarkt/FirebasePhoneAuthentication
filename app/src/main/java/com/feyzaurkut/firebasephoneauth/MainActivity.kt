3f6cd6d6d4cbf4dea227b57a6901522e9133924f
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-06 19:12:45 +0300

    add main activity and application classes

--- app/src/main/java/com/feyzaurkut/firebasephoneauth/MainActivity.kt
+++ app/src/main/java/com/feyzaurkut/firebasephoneauth/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.feyzaurkut.firebasephoneauth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val auth : FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Transparent status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
+
+        checkUserLogin()
     }
+
+    private fun checkUserLogin(){
+        if(auth.currentUser != null)
+            navController.navigate(R.id.action_loginFragment_to_homeFragment)
+    }
+
 }
